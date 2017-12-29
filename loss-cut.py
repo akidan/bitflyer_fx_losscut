8debc866ad3c72e59c83cab290552091270fc375
zhiyue.wang <zhiyue.wang@rakuten.com>
2017-12-29 14:31:23 +0900

    add usage

--- loss-cut.py
+++ loss-cut.py
@@ -1,14 +1,18 @@
 #-*- coding: UTF-8 -*-
 import os,sys
-needDeposit = float(sys.argv[1])
-hasDeposit  = float(sys.argv[2])
-btcFx = float(sys.argv[3])
-if len(sys.argv)<5:
-  times = 2.0
+if len(sys.argv) < 4 or sys.argv[1] == "-h" or sys.argv[1] == "--help":
+  print "Usage: Parameter[0] [1]       [2]        [3]       [4]      [5]"
+  print "python loss-cut.py 預入証拠金 必要証拠金 btcFX数量 [倍数=2] [買/売フラグ]"
 else:
-  times = float(sys.argv[4])
-sellFlg = 1.0
-if len(sys.argv)>6:
-  sellFlg = -1.0
-print "Add-deposit: " + str((hasDeposit*(times+sellFlg*0.8)-sellFlg*needDeposit)/btcFx)
-print "Loss-cut   : " + str((hasDeposit*(times+sellFlg*0.5)-sellFlg*needDeposit)/btcFx)
+  needDeposit = float(sys.argv[1])
+  hasDeposit  = float(sys.argv[2])
+  btcFx = float(sys.argv[3])
+  if len(sys.argv)<5:
+    times = 2.0
+  else:
+    times = float(sys.argv[4])
+  sellFlg = 1.0
+  if len(sys.argv)>5:
+    sellFlg = -1.0
+  print "Add-deposit: " + str((hasDeposit*(times+sellFlg*0.8)-sellFlg*needDeposit)/btcFx)
+  print "Loss-cut   : " + str((hasDeposit*(times+sellFlg*0.5)-sellFlg*needDeposit)/btcFx)
